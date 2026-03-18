68afce133d4530b35022e5cf9bab496b9ad59f57
Whitman <whitmandouglasphillips@gmail.com>
2026-03-17 22:34:46 -0500

    idk

--- src/main/java/net/generic_user94/nuncanulus/event/ModClientEvents.java
+++ src/main/java/net/generic_user94/nuncanulus/event/ModClientEvents.java
@@ -2,13 +2,21 @@ package net.generic_user94.nuncanulus.event;
 
 import net.generic_user94.nuncanulus.NuncAnulus;
 import net.generic_user94.nuncanulus.item.ModItems;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ComputeFovModifierEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 
 @EventBusSubscriber(modid = NuncAnulus.MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class ModClientEvents {
+
     @SubscribeEvent
     public static void onComputeFovModifierEvent(ComputeFovModifierEvent event){
 
@@ -21,6 +29,17 @@ public class ModClientEvents {
             event.setNewFovModifier(fovModifier);
         }
 
+    }
+
+    @SubscribeEvent
+    public static void livingDamage(LivingDamageEvent.Pre event) {
+        if(event.getEntity() instanceof Sheep sheep && event.getSource().getDirectEntity() instanceof Player player) {
+            if(player.getMainHandItem().getItem() == Items.END_ROD) {
+                player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a sheep with an END ROD? YOU SICK FRICK!"));
+                sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 6));
+                player.getMainHandItem().shrink(1);
+            }
+        }
     }
 
 }
